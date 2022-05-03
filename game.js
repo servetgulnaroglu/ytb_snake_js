b472a44cd4b015e1eff9895dfb8dabb3650a5f81
Nick <41508176+M1lkZ@users.noreply.github.com>
2022-05-03 22:24:18 +0300

    Update game.js

--- game.js
+++ game.js
@@ -31,14 +31,11 @@ function update() {
 }
 
 function eatApple() {
-    if (snake.tail[snake.tail.length - 1].x == apple.x &&
-        snake.tail[snake.tail.length - 1].y == apple.y) {
-        snake.tail[snake.tail.length] = {
-            x: apple.x,
-            y: apple.y
+    if(snake.tail[snake.tail.length - 1].x == apple.x &&
+        snake.tail[snake.tail.length - 1].y == apple.y){
+            snake.tail[snake.tail.length] = {x:apple.x, y: apple.y}
+            apple = new Apple();
         }
-        apple = new Apple();
-    }
 }
 
 function SnakeHitCheck() {
