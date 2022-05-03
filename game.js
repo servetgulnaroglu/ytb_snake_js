cced64460606de6694d8f271484a34deac1e9b48
Nick <41508176+M1lkZ@users.noreply.github.com>
2022-05-03 22:19:54 +0300

    Update game.js

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById("canvas")
 const canvasContext = canvas.getContext('2d')
+const HardButton = document.getElementById('HardMode')
 let HardModeOn = false
-const HardButton = document.getElementById('HardMode');
 
 
 
